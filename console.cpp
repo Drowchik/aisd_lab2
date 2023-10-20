57a3433c15b890b0703c2759d674b4e8305a944e
natal <natal@CLOWN>
2023-10-21 00:58:50 +0400

    everything_except_delete_node

--- console.cpp
+++ console.cpp
@@ -3,24 +3,14 @@
 using namespace std;
 using namespace Tlink;
 
-int main() {
-	LinkedList<int> a;
+void sum(LinkedList<int>& a) {
 	LinkedList<int> c;
-	a.push_head(5);
-	a.push_head(6);
-	a.push_head(7);
-	a.print();
-	a.pop_head();
-	a.pop_head();
-	a.print();
-	c.push_head(3);
-	c.push_tail(1);
-	c.print();
-	a.print();
-	LinkedList<int> b(c);
-	b.print();
-	a = c;
-	a.print();
+	c.push_head(7);
+	c.push_head(7);
+	c.push_head(7);
+	a.push_head(c);
+}
+int main() {
 
 
 	return 0;
--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -18,6 +18,7 @@ namespace Tlink{
 		Node<T>* _head;
 	public:
 		LinkedList() : _head(nullptr) {}
+
 		LinkedList(const LinkedList& a) : _head(nullptr) {
 			Node<T>* ptr = a._head;
 			while (ptr) {
@@ -25,6 +26,8 @@ namespace Tlink{
 				ptr = ptr->_next;
 			}
 		}
+
+
 		~LinkedList() {
 			while (_head)
 			{
@@ -32,20 +35,26 @@ namespace Tlink{
 				_head = _head->_next;
 				delete ptr;
 			}
+
 		}
+
 		LinkedList& operator=(LinkedList a) {
 			swap(a);
 			return* this;
 		}
+
 		void swap(LinkedList& a) {
 			std::swap(_head, a._head);
 		}
+
+
 		void push_head(const T& data) {
 			_head = new Node<T>(data, _head);
 		}
+
 		void push_tail(const T& data) {
 			if (!_head) {
-				_head= new Node<T>(data);
+				this->push_head(data);
 				return;
 			}
 			Node<T>* ptr = _head;
@@ -54,27 +63,80 @@ namespace Tlink{
 			}
 			ptr->_next = new Node<T>(data);
 		}
+
+		void push_head(LinkedList a) {
+			a.push_tail(*this);
+			swap(a);
+		}
+
+		void push_tail(const LinkedList& a) {
+			Node<T>* ptr = a._head;
+			while (ptr) {
+				this->push_tail(ptr->_data);
+				ptr = ptr->_next;
+			}
+		}
+
 		void pop_head() {
+			if (!_head) {
+				throw out_of_range("List emplty, but you are trying delete data.");
+			}
 			Node<T>* ptr = _head;
 			_head = _head->_next;
 			delete ptr;
 			ptr = nullptr;
 		}
-		//void delete_node(T data) {
+
+		void pop_tail() {
+			if (!_head) {
+				throw out_of_range("List emplty, but you are trying delete data.");
+			}
+			if (!_head->_next) {
+				delete _head;
+				_head = nullptr;
+				return;
+			}
+			Node<T>* ptr = _head;
+			while (ptr->_next->_next) {
+				ptr = ptr->_next;
+			}
+			Node<T>* temp = ptr;
+			delete ptr->_next;
+			temp->_next = nullptr;
+
+		}
+
+		const T& operator[](size_t index) const {
+			auto ptr = _head;
+			while (index>0 && ptr) {
+				ptr = ptr->_next;
+				index--;
+			}
+			if (!ptr) {
+				throw std::out_of_range("Index out of range");
+			}
+			return ptr->_data;
+		}
+
+		T& operator[](size_t index) {
+			auto ptr = _head;
+			while (index > 0 && ptr) {
+				ptr = ptr->_next;
+				index--;
+			}
+			if (!ptr) {
+				throw std::out_of_range("Index out of range");
+			}
+			return ptr->_data;
+
+		}
+		//void delete_node(const T& data) {
 		//	Node<T>* ptr = _head;
 		//	while (ptr) {
 		//		if (ptr->_data == data) {
-
+		//			auto temp = ptr;
 		//		}
 		//	}
-		//}
-		//void push_head(LinkedList& a) {
-		//	Node<T>* ptr = a._head;
-		//	while (ptr->_next) {
-		//		ptr = ptr->_next;
-		//	}
-		//	ptr->_next = _head;
-		//	_head = a._head;
 		//}
 		ostream& print(ostream& os = cout) const {
 			Node<T>* ptr = _head;
